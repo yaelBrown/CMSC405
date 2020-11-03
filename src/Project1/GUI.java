42325c6c4fc987e66453b9e00d7ef8774122971b
Yael Brown <yaelrbrown@gmail.com>
2020-11-02 19:38:51 -0600

    finished project 1

--- src/Project1/GUI.java
+++ src/Project1/GUI.java
@@ -16,13 +16,13 @@ public class GUI {
         frame = new JFrame("Java 2D Graphics");
         panel = new JPanel();
 
-        frame.setSize(300, 300);
+        frame.setSize(300, 900);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocation((screen.width - frame.getWidth()) / 2, (screen.height - frame.getHeight())/2 );
-        frame.setVisible(true);
 
-//        frame = gp1;
         frame.add(gp1);
+
+        frame.setVisible(true);
     }
 
 
--- src/Project1/Graphics.java
+++ src/Project1/Graphics.java
@@ -11,11 +11,58 @@ public class Graphics extends JPanel{
 
         Graphics2D g2D = (Graphics2D) g;
 
-        g2D.setColor(Color.GREEN);
-        g2D.setStroke(new BasicStroke(20));
-        g2D.drawLine(0,0,300,300);
+        createLine(g2D);
+        createTriangle(g2D);
+        createSquare(g2D);
     }
 
+    // Crossing lines
+    private void createLine(Graphics2D out) {
+        out.setColor(Color.GREEN);
+        out.setStroke(new BasicStroke(20));
+        out.drawLine(0,0,300,300);
+        out.drawLine(300,0,0,300);
+    }
 
+    // Creates a square
+    private void createSquare(Graphics2D g) {
+        g.setColor(Color.RED);
+        g.setStroke(new BasicStroke(20));
 
+        int[] x = {50, 250, 250, 50};
+        int[] y = {650, 650, 850, 850};
+
+        g.drawPolygon(x, y, 4);
+    }
+
+    // Creates a triangle
+    private void createTriangle(Graphics2D g) {
+        g.setColor(Color.YELLOW);
+        g.setStroke(new BasicStroke(20));
+
+        int[] x = {50, 150, 250};
+        int[] y = {580, 400, 580};
+
+        g.drawPolygon(x, y, 3);
+    }
+
+    // Translate the images
+    private void translateImages(Graphics2D g) {
+        g.translate(-5, +7);
+    }
+
+    // Rotate the images 45 degrees
+    private void rotateImages45(Graphics2D g) {
+        g.rotate(45.0);
+    }
+
+    // Rotate the images 90 degrees
+    private void rotateImages90(Graphics2D g) {
+        g.rotate(90.0);
+    }
+
+    // Scale the images
+    private void scaleImages(Graphics2D g) {
+        g.scale(2, 0.5);
+    }
 }
